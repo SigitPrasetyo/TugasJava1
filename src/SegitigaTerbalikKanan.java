616f451d2db693607e3749ca695d6dc2690bdb10
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 23:03:00 +0700

    12141384 masukan angka dengan System.out.println (“Masukan Angka : “);untuk mencetak output karakter masukan angka, kemudian a = input.nextint();yang berarti variabel a merupakan nilai dari input bernilai integer yang dimasukkan setelah karakter masukan angka tadi.

--- src/SegitigaTerbalikKanan.java
+++ src/SegitigaTerbalikKanan.java
@@ -9,4 +9,19 @@ class SegitigaTerbalikKanan {
        panjang= input.nextInt();
 
         System.out.println("**********************************");
-        
+        for (b=panjang; b>=0; b--)
+           {
+             for (a=0; a<=b; a++)
+            {
+             System.out.print(a);
+             }
+             System.out.println ();
+             for (int o=panjang; o>=b; o--)
+            {
+            System.out.print (" ");
+            } 
+          } 
+          
+       }
+}    
+         
