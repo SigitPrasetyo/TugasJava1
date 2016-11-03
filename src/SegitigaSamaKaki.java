590a745a1269efe1bc167d4fe6fc2d1dd3ad4f1e
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 19:57:44 +0700

    12141384 Input batasan karakter

--- src/SegitigaSamaKaki.java
+++ src/SegitigaSamaKaki.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class SegitigaSamaKaki {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       int panjang;
+        System.out.printf("Jumlah Karakter/Tinggi Segitiga:");
+       panjang= input.nextInt();
+
+//        
+    }
 }
 
