761f30596c07ee6f2c2c231b4874d82369f5594f
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 22:44:13 +0700

    12141384 gunakan Scanner input = new Scanner(System.in);yang artinya  menggunakan nama input sebagai scanner atau penyalinan dari input data yang nantinya dimasukkan pada output program. Lalu definisikan tipe data i,k,; sebagai integer dengan int i,k;

--- src/SegitigaSikuTerbalikKiri.java
+++ src/SegitigaSikuTerbalikKiri.java
@@ -3,4 +3,9 @@ import java.util.Scanner;
 class SegitigaSikuTerbalikKiri {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int i,k;
+        int panjang;
+        System.out.printf("Jumlah Karakter/Tinggi Segitiga:");
+        panjang = input.nextInt();
+
+//        System.out.print("=");
