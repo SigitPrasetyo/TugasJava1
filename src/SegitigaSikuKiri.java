acf347463c9c02ecc78ab34e98f46a4f99f05f59
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 22:29:18 +0700

    12141384 tipe data a,b; sebagai integer dengan int a,b; dan panjang untuk panjang segitiga. angka dengan System.out.println (“Masukan Angka : “) output karakter masukan angka, kemudian a = input.nextint();yang berarti variabel a merupakan nilai dari input bernilai integer yang dimasukkan setelah karakter masukan angka tadi.

--- src/SegitigaSikuKiri.java
+++ src/SegitigaSikuKiri.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 class SegitigaSikuKiri {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int a,b;
+       int panjang;
+        System.out.printf("Jumlah Karakter/Tinggi Segitiga:");
+       panjang= input.nextInt();
+
+        System.out.print("**********************************");
+        // TODO code application logic here
        
            
     
