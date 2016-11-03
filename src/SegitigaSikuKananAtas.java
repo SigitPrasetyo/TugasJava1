7d4686b044da6ab230d4f873fb7540912c4483bd
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 21:46:54 +0700

    12141384 Kemudian seperti biasa gunakan public class segitiga{untuk membuat judul pada class bernama segitiga seperti nama program yang kita buat. Untuk membuat main program atau blok utama program,gunakan public static void main (String [] args) { sama seperti setiap membuat program dengan bahasa java,slalu wajib menggunakan statement tersebut untuk membuat blok inti program.Kemudian pada blok inti tersebut,gunakan Scanner input = new Scanner(System.in); yang artinya menggunakan nama input sebagai scanner atau penyalinan dari input data yang nantinya dimasukkan pada output program. Lalu definisikan tipe data i,j;sebagai integer dengan int i,j;

--- src/SegitigaSikuKananAtas.java
+++ src/SegitigaSikuKananAtas.java
@@ -3,4 +3,9 @@ import java.util.Scanner;
 class SegitigaSikuKananAtas {
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
-       
+       int i,j;
+       int panjang;
+       System.out.printf("Jumlah Karakter/Tinggi Segitiga:");
+       panjang= input.nextInt();
+
+        System.out.println("**********************************");
