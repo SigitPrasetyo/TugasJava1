6780d9d4e2e2eb3be68f6e4103a39b92c4aca286
sigit23 <prasetyosigit23@gmail.com>
2016-11-03 22:23:29 +0700

    12141384 membuat program java yang akan menampilkan bentuk Segitiga SikuKiri dengan menggunakan perulangan pada bilangan. mengambil semua library yang ada pada package util pada java dengan menggunakan import java.util.*;.gunakan public class segitiga {untuk membuat judul pada class bernama segitiga seperti nama program. Untuk membuat main program atau blok utama program,gunakan public static void main (String [] args) { sama seperti setiap membuat program dengan bahasa java. gunakan Scanner input = new Scanner(System.in); yang artinya menggunakan nama input sebagai scanner atau penyalinan dari input data yang nantinya dimasukkan pada output program

--- src/SegitigaSikuKiri.java
+++ src/SegitigaSikuKiri.java
@@ -3,23 +3,7 @@ import java.util.Scanner;
 class SegitigaSikuKiri {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a,b;
-       int panjang;
-        System.out.printf("Jumlah Karakter/Tinggi Segitiga:");
-       panjang= input.nextInt();
-
-        System.out.print("**********************************");
-        // TODO code application logic here
-
-        for (a = 0; a <=panjang; a++) {
-           for (b = 0 ; a > b; b++) {
-                System.out.print(b);
-            }
-           System.out.print("\n");
-        }
-            System.out.println();
-    }
-}
+        
        
            
     
